6d1b9e1bb7a00ff181ea767fb513dc3a26e60f4d
svemargit <svejdam@icloud.com>
2023-08-05 16:11:13 +0200

    exercise 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+import src.exercises.Training;
+
+public class Main {
+    public static void main(String[] args) {
+        /* nothing for main class at the moment */
+        System.out.println("This is a Main class, starting up...");
+        new Training().spinWords("Welcome to my world");
+    }
+}
