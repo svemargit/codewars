9eb54183e2a8b5ee5ffa50f7cc2e939cca9e852d
svemargit <svejdam@icloud.com>
2023-08-05 18:16:56 +0200

    second taks, only assignment

--- src/exercises/Training.java
+++ src/exercises/Training.java
@@ -3,19 +3,37 @@ package src.exercises;
 import java.util.ArrayList;
 import src.Main;
 
-/**
- * Write a function that takes in a string of one or more words, and returns the same string, but
- * with all five or more letter words reversed (Just like the name of this Kata). Strings passed in
- * will consist of only letters and spaces. Spaces will be included only when more than one word is
- * present.
- *
- * <p>Examples:
- *
- * <p>spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a
- * test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona
- * test"
- */
 public class Training extends Main {
+  /**
+   * You probably know the "like" system from Facebook and other pages. People can "like" blog
+   * posts, pictures or other items. We want to create the text that should be displayed next to
+   * such an item.
+   *
+   * <p>Implement the function which takes an array containing the names of people that like an
+   * item. It must return the display text as shown in the examples:
+   *
+   * <p>[] --> "no one likes this" ["Peter"] --> "Peter likes this" ["Jacob", "Alex"] --> "Jacob and
+   * Alex like this" ["Max", "John", "Mark"] --> "Max, John and Mark like this" ["Alex", "Jacob",
+   * "Mark", "Max"] --> "Alex, Jacob and 2 others like this" Note: For 4 or more names, the number
+   * in "and 2 others" simply increases.
+   */
+  public static String whoLikesIt(String... names) {
+    // Do your magic here
+    return "";
+  }
+
+  /**
+   * Write a function that takes in a string of one or more words, and returns the same string, but
+   * with all five or more letter words reversed (Just like the name of this Kata). Strings passed
+   * in will consist of only letters and spaces. Spaces will be included only when more than one
+   * word is present.
+   *
+   * <p>Examples:
+   *
+   * <p>spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a
+   * test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is
+   * rehtona test"
+   */
   public String spinWords(String sentence) {
     StringBuilder spunSentence = new StringBuilder();
     long spaceCount = sentence.chars().filter(c -> c == (int) ' ').count();
